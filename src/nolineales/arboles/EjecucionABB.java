1e70b504b286b5f17cccbd96c8c235d4cfcd7e63
zarate10 <zaratelautaro999@gmail.com>
2023-07-27 10:03:47 -0300

    commit

--- src/nolineales/arboles/EjecucionABB.java
+++ src/nolineales/arboles/EjecucionABB.java
@@ -133,6 +133,70 @@ public class EjecucionABB {
         return mayores;
     }
 
+    public int calcularProfundidadNodo(TDAABB arbol, int nodo)
+    {
+        if (arbol.ArbolVacio())
+            return -1;
+
+        if (arbol.Raiz() == nodo)
+            return 0;
+
+        if(nodo < arbol.Raiz())
+            return 1 + calcularProfundidadNodo(arbol.HijoIzq(), nodo);
+        else
+            return 1 + calcularProfundidadNodo(arbol.HijoDer(), nodo);
+    }
+
+    public IConjunto descendientesNodo(TDAABB arbol, int nodo)
+    {
+        IConjunto descendientes = new Conjunto();
+        descendientes.inicializar();
+
+        if(!arbol.ArbolVacio()) {
+            if (nodo < arbol.Raiz())
+                return descendientesNodo(arbol.HijoIzq(), nodo);
+
+            if (nodo > arbol.Raiz())
+                return descendientesNodo(arbol.HijoDer(), nodo);
+
+            if (arbol.Raiz() == nodo)
+            {
+                if (!arbol.HijoIzq().ArbolVacio())
+                {
+                    descendientes.agregar(arbol.HijoIzq().Raiz());
+                    unirConjunto(descendientes, descendientesNodo(arbol.HijoIzq(), arbol.HijoIzq().Raiz()));
+                }
+
+                if (!arbol.HijoDer().ArbolVacio())
+                {
+                    descendientes.agregar(arbol.HijoDer().Raiz());
+                    unirConjunto(descendientes, descendientesNodo(arbol.HijoDer(), arbol.HijoDer().Raiz()));
+                }
+            }
+        }
+
+        return descendientes;
+    }
+
+    public int sizeABB(TDAABB arbol)
+    {
+        if (arbol.ArbolVacio())
+            return 0;
+
+        return (1 + sizeABB(arbol.HijoIzq())) + sizeABB(arbol.HijoDer());
+    }
+
+    public int calcularSumaElementosABB(TDAABB arbol)
+    {
+        int suma = 0;
+
+        if (arbol.ArbolVacio())
+            return 0;
+
+        suma += arbol.Raiz();
+        return suma + calcularSumaElementosABB(arbol.HijoIzq()) + calcularSumaElementosABB(arbol.HijoDer());
+    }
+
     public EjecucionABB() {
         TDAABB a = new ABB();
         a.InicializarArbol();
@@ -145,14 +209,18 @@ public class EjecucionABB {
         a.AgregarElem(37);
         a.AgregarElem(43);
 
-        //System.out.println(altura(a));
+        // System.out.println(altura(a));
 
         // TDAABB raizHijoDer = hijoDerechoNodo(a, 23);
 
-        //System.out.println(padreNodoDado(a, 42).Raiz());
-        //System.out.println(esABB(a));
+        // System.out.println(padreNodoDado(a, 42).Raiz());
+        // System.out.println(esABB(a));
 
         // printConjunto(paresArbol(a));
-        printConjunto(numerosMayores(a, 42));
+        // printConjunto(numerosMayores(a, 42));
+        // System.out.println(calcularProfundidadNodo(a, 43));
+        // printConjunto(descendientesNodo(a, 42));
+        // System.out.println(sizeABB(a));
+        System.out.println(calcularSumaElementosABB(a));
     }
 }
